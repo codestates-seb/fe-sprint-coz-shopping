94f1897e3a85b564ea1f939d5df6d8ddaa6ccced
coaudtn0276 <coaudtn0276@gmail.com>
2023-05-15 18:16:24 +0900

    dropdown menu목업, 상단바 고정

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 // import { Routes, Route, Link } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
@@ -8,6 +11,34 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Container>
+        <Row>
+          <Col sm>sm=true</Col>
+          <Col sm>sm=true</Col>
+          <Col sm>sm=true</Col>
+          <Col sm>sm=true</Col>
+        </Row>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </Container>
 
       <Footer />
     </div>
--- shopping/src/components/header.js
+++ shopping/src/components/header.js
@@ -1,14 +1,43 @@
 import logo from "../img/로고.svg";
-import logo2 from "../img/햄버거 버튼.svg";
+import hamburger from "../img/햄버거 버튼.svg";
+import dropdown from "../img/Dropdown.svg";
+import gift from "../img/상품 아이콘.svg";
+import star from "../img/북마크 아이콘.svg";
+import { useState } from "react";
 
 const Header = function Header() {
+  const [drop, setDrop] = useState(false);
+
   return (
     <div className="header">
       <div className="header-2">
         <img src={logo} alt="" className="img" />
         <span className="navtitle">COZ Shopping</span>
       </div>
-      <img src={logo2} alt="" className="img2" />
+      <div className="dropdown">
+        <img
+          src={hamburger}
+          alt=""
+          className="img2"
+          onClick={() => {
+            setDrop(!drop);
+          }}
+        />
+        {drop === true ? (
+          <div className="dropdown">
+            <img src={dropdown} alt="" className="dropdownimg" />
+            <div className="dropdowntitle-1">OOO님, 안녕하세요</div>
+            <div className="dropdowntitle-2">
+              <img src={gift} alt="" className="icon" />
+              상품리스트 페이지
+            </div>
+            <div className="dropdowntitle-3">
+              <img src={star} alt="" className="icon" />
+              북마크 페이지
+            </div>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
