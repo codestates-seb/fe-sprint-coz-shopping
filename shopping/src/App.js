0aede1cf9a38e7e9bf1dd65fd44e239dfce5351c
coaudtn0276 <coaudtn0276@gmail.com>
2023-05-15 14:28:57 +0900

    footer목업

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -1,14 +1,15 @@
-// import logo from "./img/로고.svg";
-// import logo2 from "./img/햄버거 버튼.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route, Link } from "react-router-dom";
+// import { Routes, Route, Link } from "react-router-dom";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Footer />
     </div>
   );
 }
---
+++ shopping/src/components/footer.js
@@ -0,0 +1,11 @@
+const Footer = function Footer() {
+  return (
+    <div className="footer">
+      개인정보 처리방침 | 이용 약관
+      <br />
+      All rights reserved @ Codestates
+    </div>
+  );
+};
+
+export default Footer;
