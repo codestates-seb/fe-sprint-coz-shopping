ba79977e01017ee25bb6e210cab07829ee8dc46d
Xiel0112 <guswl000112@gmail.com>
2023-05-16 20:42:37 +0900

    Main 페이지 구성

---
+++ coz-shopping/src/page/Main.js
@@ -0,0 +1,22 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import List from "../components/List";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <Header />
+      <div className="main__list">
+        <div className="main__title">상품 리스트</div>
+        <List />
+      </div>
+      <div className="main__list">
+        <div className="main__title">북마크</div>
+        <List />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
