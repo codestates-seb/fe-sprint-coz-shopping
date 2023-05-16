420d6af6f3823a93a6f742d5a57709d32d8614ed
Xiel0112 <guswl000112@gmail.com>
2023-05-16 22:32:24 +0900

    List 컴포넌트 구성

--- coz-shopping/src/components/List.js
+++ coz-shopping/src/components/List.js
@@ -1,5 +1,16 @@
+import iconBookmark from "../icon/북마크 아이콘.png";
+
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div>
+      <img src="" alt="product" />
+      <img src={iconBookmark} alt="bookmark" />
+      <span>상품명</span>
+      <span>할인률</span>
+      <span>부가 설명</span>
+      <span>가격 / 관심고객수 / null</span>
+    </div>
+  );
 };
 
 export default List;
