23ca9bf951f2e3b6654a39ddf18c9fbef1861a7c
Xiel0112 <guswl000112@gmail.com>
2023-05-16 21:34:00 +0900

    Dropdown 클릭 시, 페이지 이동

--- coz-shopping/src/components/Header.js
+++ coz-shopping/src/components/Header.js
@@ -1,13 +1,26 @@
+import { Link } from "react-router-dom";
 import logo from "../icon/로고.png";
-import icon from "../icon/아이콘.png";
+import iconMenu from "../icon/아이콘.png";
+import iconProduct from "../icon/상품 아이콘.png";
+import iconBookmark from "../icon/북마크 아이콘.png";
 
 const Header = () => {
   const Dropdown = () => {
     return (
       <div className="dropdown">
-        <li className="dropdown__welcome">○○○님, 안녕하세요</li>
-        <li className="dropdown__list">상품리스트 페이지</li>
-        <li className="dropdown__list">북마크</li>
+        <div className="dropdown__welcome">○○○님, 안녕하세요</div>
+        <Link className="dropdown__list" to="/products/list">
+          <div>
+            <img className="dropdown__icon" src={iconProduct} alt="icon" />
+            상품리스트 페이지
+          </div>
+        </Link>
+        <Link className="dropdown__list" to="/bookmark">
+          <div>
+            <img className="dropdown__icon" src={iconBookmark} alt="icon" />
+            북마크 페이지
+          </div>
+        </Link>
       </div>
     );
   };
@@ -16,7 +29,7 @@ const Header = () => {
     <div className="header">
       <img className="header__logo" src={logo} alt="logo" />
       <h1 className="header__shoppingMall">COZ Shopping</h1>
-      <img className="header__menu" src={icon} alt="menu" />
+      <img className="header__menu" src={iconMenu} alt="menu" />
       <Dropdown />
     </div>
   );
