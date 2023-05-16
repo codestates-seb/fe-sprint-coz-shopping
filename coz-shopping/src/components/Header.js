aa3e3a3c715f89e3a84319878ed5c8c4ac07cb8f
Xiel0112 <guswl000112@gmail.com>
2023-05-16 21:16:26 +0900

    Header 구성

--- coz-shopping/src/components/Header.js
+++ coz-shopping/src/components/Header.js
@@ -1,5 +1,25 @@
+import logo from "../icon/로고.png";
+import icon from "../icon/아이콘.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  const Dropdown = () => {
+    return (
+      <div className="dropdown">
+        <li className="dropdown__welcome">○○○님, 안녕하세요</li>
+        <li className="dropdown__list">상품리스트 페이지</li>
+        <li className="dropdown__list">북마크</li>
+      </div>
+    );
+  };
+
+  return (
+    <div className="header">
+      <img className="header__logo" src={logo} alt="logo" />
+      <h1 className="header__shoppingMall">COZ Shopping</h1>
+      <img className="header__menu" src={icon} alt="menu" />
+      <Dropdown />
+    </div>
+  );
 };
 
 export default Header;
