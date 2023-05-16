95f71a29c43061a9b56626130c633261961be4d1
Xiel0112 <guswl000112@gmail.com>
2023-05-16 21:58:09 +0900

    Footer 내용 가운데 정렬

--- coz-shopping/src/components/Footer.js
+++ coz-shopping/src/components/Footer.js
@@ -1,8 +1,10 @@
+import "../components/Footer.css";
+
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer__content">개인정보 처리방침 | 이용 약관</div>
-      <div className="footer__content">All rights reserved @ Codestates</div>
+      <div className="footer__context">개인정보 처리방침 | 이용 약관</div>
+      <div className="footer__context">All rights reserved @ Codestates</div>
     </div>
   );
 };
