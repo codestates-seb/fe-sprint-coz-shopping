6c29d26a97bcc15aa588b4d905106710db1798fe
Xiel0112 <guswl000112@gmail.com>
2023-05-16 23:01:30 +0900

    modal 컴포넌트

--- coz-shopping/src/components/List.js
+++ coz-shopping/src/components/List.js
@@ -4,21 +4,42 @@ import iconBookmarkOn from "../icon/북마크 아이콘 - on.png";
 
 const List = () => {
   const [isBookmark, setIsBookmark] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
   const handleBookmark = () => setIsBookmark(!isBookmark);
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseMoal = () => setOpenModal(false);
+
+  const Modal = () => {
+    return (
+      <div className="modal">
+        <img className="modal__img" src="" alt="Production" />
+        <button className="modal__button" onClick={handleCloseMoal}>
+          X
+        </button>
+      </div>
+    );
+  };
 
   return (
     <div>
-      <img src="" alt="product" />
-      <img
-        src={isBookmark ? iconBookmarkOn : iconBookmark}
-        alt="bookmark"
-        onClick={handleBookmark}
-      />
-      <span>상품명</span>
-      <span>할인률</span>
-      <span>부가 설명</span>
-      <span>가격 / 관심고객수 / null</span>
+      <div>
+        <img src="" alt="product" onClick={handleOpenModal} />
+        <img
+          src={isBookmark ? iconBookmarkOn : iconBookmark}
+          alt="bookmark"
+          onClick={handleBookmark}
+        />
+      </div>
+      <div>
+        <span>상품명</span>
+        <span>할인률</span>
+      </div>
+      <div>
+        <span>부가 설명</span>
+        <span>가격 / 관심고객수 / null</span>
+      </div>
+      {openModal && <Modal />}
     </div>
   );
 };
