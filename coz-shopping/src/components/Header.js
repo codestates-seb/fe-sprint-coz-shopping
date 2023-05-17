dc5867979130c9250fd3f49f5c4b4352c54e0d57
Scarlett_JS <scarlett.js1010@gmail.com>
2023-05-17 15:58:53 +0900

    Header import 수정

--- coz-shopping/src/components/Header.js
+++ coz-shopping/src/components/Header.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import useState from "react";
+import {Components, useState} from "react";
 import styled from 'styled-components';
 
 const CustomHeader = styled.header`
