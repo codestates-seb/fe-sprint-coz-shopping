520718ccc0a35e9ab1dff6a5aa45f8dd93e1d7a2
Scarlett_JS <scarlett.js1010@gmail.com>
2023-05-17 15:52:05 +0900

    styled-components적용

--- coz-shopping/src/components/Header.js
+++ coz-shopping/src/components/Header.js
@@ -1,6 +1,75 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import "../styles/Header.css"
+import useState from "react";
+import styled from 'styled-components';
+
+const CustomHeader = styled.header`
+    position: fixed;
+    width: 100%;
+    height: 9rem;
+    background-color: #ffffff;
+    display: flex;
+    align-items: center;
+    padding: 0 8rem 0 0;
+    box-shadow: 0px 2px 4px 4px rgba(0, 0, 0, 0.2);
+
+    >span {
+        font-size: 3.5rem;
+        font-weight: 700;
+        margin-right: auto;
+        margin-bottom: 0.3rem;
+    }
+
+    .logo>img {
+        width: 7rem;
+        height: 8rem;
+        margin-right: 1.5rem;
+        margin-left: 10rem;
+    }
+`
+
+const Toggle = styled.button`
+    cursor: pointer;
+    padding-right: 3rem;
+    
+    >img {
+        width: 4rem;
+        padding: 1.2rem 0 0 0;
+    }
+`
+
+const Modal = styled.div`
+    position: fixed;
+    top: 7rem;
+    right: 13rem;
+    display: flex;
+    justify-content: right;
+
+    >div {
+        display: flex;
+        text-align: center;
+        position: absolute;
+        flex-direction: column;
+        justify-content: space-evenly;
+        width: 27rem;
+        height: 20rem;
+        background-color: #ffffff;
+        padding: 1.5rem;
+        border-radius: 12px;
+        filter: drop-shadow(0px 0px 8px rgba(0, 0, 0, 0.3));
+        >span {
+            font-size: 2.3rem;
+        }
+        >a>img {
+            width: 2.3rem;
+        }
+        >a>span {
+            font-size: 2.3rem;
+            padding: 0.5rem 0.7rem;
+        }
+    }
+
+    
+`
 
 function Header() {
     const [isToggleOpen, setIsToggleOpen] = useState(false);
@@ -10,20 +79,18 @@ function Header() {
     };
 
     return (
-        <header className="header">
+        <CustomHeader className="header">
             <Link to="/" className="logo">
                 <img src={process.env.PUBLIC_URL + 'images/icon/codestates_icon.svg'} alt="logo" />
             </Link>
             <span>COZ Shopping</span>
-            <div className="toggle-container">
-                <button className="toggle" onClick={handleToggleOpen}>
-                    <img src={process.env.PUBLIC_URL + 'images/icon/toggle.svg'} alt="toggle" />
-                </button>
-            </div>
+            <Toggle className="toggle" onClick={handleToggleOpen}>
+                <img src={process.env.PUBLIC_URL + 'images/icon/toggle.svg'} alt="toggle-img" />
+            </Toggle>
             {isToggleOpen ? (
-                <div className="modal-overlay">
+                <Modal>
                     <div className="modal">
-                        <div>OOO님 환영합니다.</div>
+                        <span>OOO님 환영합니다.</span>
                         {/* 상품 리스트 걸기 */}
                         <Link to={''} className="pd_list_icon">
                             <img src={process.env.PUBLIC_URL + 'images/icon/pd_list_icon.svg'} alt="toggle" />
@@ -35,9 +102,9 @@ function Header() {
                             <span>북마크 리스트</span>
                         </Link>
                     </div>
-                </div>
+                </Modal>
             ) : null}
-        </header>
+        </CustomHeader>
     )
 };
 
