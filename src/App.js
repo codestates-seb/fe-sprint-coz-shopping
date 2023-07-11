a819f72e980a8c1920aabef461ec13c78a9317e6
myeongin <mungin0926@naver.com>
2023-07-11 15:11:59 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import BookMark from "./pages/BookMark";
+import MainPage from "./pages/MainPage";
+import ProductListPage from "./pages/ProductListPage";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/bookmark" element={<BookMark />} />
+          <Route path="/products/list" element={<ProductListPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
