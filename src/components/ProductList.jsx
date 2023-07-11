caeeceb07adc56ff650adde0e1b73c426127df47
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-12 00:30:11 +0900

    product list api call

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./productlist.css";
+import { productsLimitFour } from "../shared/products";
 
 export const ProductList = () => {
+  const [viewProductList, setViewProductList] = useState([]);
+  const productList = productsLimitFour;
+
+  useEffect(() => {
+    fetch(productList) //
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setViewProductList(data);
+      });
+  }, []);
+
   return (
     <article>
       <h1>상품 리스트</h1>
