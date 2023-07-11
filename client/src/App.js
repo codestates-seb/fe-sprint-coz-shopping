e2b0151cabbe928a14d86a63adced7d8faf52f53
son-young-jin <sdudwls128@naver.com>
2023-07-11 15:55:36 +0900

    Test: branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './components/pages/MainPage';
+import BookmarkPage from './components/pages/BookmarkPage';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/bookmark" element={<BookmarkPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
