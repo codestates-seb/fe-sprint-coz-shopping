db7c3d0fec862023cbcbeff6fe5e504b429e4208
Dr-eong <rwg3465@gmail.com>
2023-07-12 01:27:48 +0900

    헤더 컴포넌트 작성  ..라우터 연결  해야함

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,59 @@
-function Header() {}
+import { styled } from "styled-components";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import DropdownMenu from "./DropdownMenu";
+
+const HeaderContainer = styled.header`
+  width: 1280px;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0px 8px 8px 0px #0000001a;
+
+  span {
+    font-size: 32px;
+    font-weight: 700;
+    line-height: 28px;
+    letter-spacing: 0em;
+    text-align: left;
+  }
+  img {
+    width: 55px;
+    height: 30px;
+    margin-left: 76px;
+    margin-right: 12px;
+  }
+  .right > .burger-icon {
+    width: 34px;
+    height: 24px;
+    margin-right: 76px;
+  }
+  .right > .burger-icon:hover {
+    cursor: pointer;
+  }
+`;
+
+//FIXME
+const openDropdownMenu = () => {
+  console.log("evnet");
+};
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <div className="left">
+        <img src="img/logo.png" alt="logo"></img>
+        <span className="header_title">COZ Shopping</span>
+      </div>
+      <div className="right">
+        <FontAwesomeIcon
+          icon={faBars}
+          className="burger-icon"
+          onClick={openDropdownMenu}
+        />
+      </div>
+    </HeaderContainer>
+  );
+}
 export default Header;
