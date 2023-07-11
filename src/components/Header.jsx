70a2c2761178240e6aeb275b885d7de4ae4a165e
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-11 17:59:11 +0900

    dropdown onClick added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import React from "react";
 import "./header.css";
 import { FaBars } from "react-icons/fa6";
+import { useState } from "react";
 
 export const Header = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
     <nav>
       <div className="nav__items">
@@ -11,14 +13,20 @@ export const Header = () => {
           <h1>COZ Shopping</h1>
         </div>
         <div className="nav__toggle">
-          <FaBars size={34} />
-          <ul className="toggle__list">
-            <li className="toggle__list__item">이지원님, 안녕하세요!</li>
-            <li className="toggle__list__item" id="item__middle">
-              상품리스트 패이지
-            </li>
-            <li className="toggle__list__item">북마크 페이지</li>
-          </ul>
+          <FaBars
+            size={34}
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          />
+
+          {isDropdownOpen && (
+            <ul className="toggle__list">
+              <li className="toggle__list__item">이지원님, 안녕하세요!</li>
+              <li className="toggle__list__item" id="item__middle">
+                상품리스트 패이지
+              </li>
+              <li className="toggle__list__item">북마크 페이지</li>
+            </ul>
+          )}
         </div>
       </div>
     </nav>
