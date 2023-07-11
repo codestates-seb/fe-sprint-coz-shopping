5b8a9ccd247ff7d327c4181d23689c3e12b3ae84
yebin <yebind3308@gmail.com>
2023-07-11 17:54:59 +0900

    🎨 header, footer 컴포넌트 파일 생성 및 개선

---
+++ src/component/Footers.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import React, { useState } from 'react';
+
+// 푸터
+const Footer = styled.footer`
+
+    .footerWrap {
+        height:58px;
+        /* width:1280px; */
+        margin:auto;
+        border-top:solid 1px black;
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+        > span {
+            height:18px;
+            font-size : 12px;
+            color : #888888;
+            display:flex;
+            flex-direction: column;
+            align-items: center;
+        }
+    }
+
+`
+
+const Footers = () => {
+
+    return (
+        <>
+            <Footer>
+                <div className="footerWrap">
+                    <span>개인정보 처리방침 | 이용 약관</span>
+                    <span>All rights reserved @ Codestates</span>
+                </div>
+            </Footer>
+        </>
+
+    );
+};
+
+export default Footers;
