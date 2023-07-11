8140520d2fea25f9a0395f4d11233160b6370650
LeeJongBum <qja965@gmail.com>
2023-07-11 16:14:35 +0900

    [#7] 라우팅 구현 및 리덕스 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 import styled from "styled-components";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./router/Main";
+import Products from "./router/Products";
+import Bookmark from "./router/Bookmark";
+import { useSelector, useDispatch } from "react-redux";
+import { updateAllProducts } from "./features/AllProductsSlice";
+import { updateLoding } from "./features/Loding";
+import { useEffect } from "react";
 const Div = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
 function App() {
-  return <Div className="App"></Div>;
+  const AllProducts = useSelector((state) => state.AllProducts);
+  const isLoding = useSelector((state) => state.Loding);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch("http://cozshopping.codestates-seb.link/api/v1/products")
+      .then((el) => el.json())
+      .then((el) => {
+        dispatch(updateAllProducts(el));
+        dispatch(updateLoding(!isLoding));
+      });
+  }, []);
+
+  console.log(AllProducts);
+  console.log(isLoding);
+
+  return (
+    <BrowserRouter>
+      <Div className="App">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/products/list" element={<Products />} />
+          <Route path="/bookmark" element={<Bookmark />} />
+        </Routes>
+      </Div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
