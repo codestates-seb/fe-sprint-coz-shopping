35c1c9af510a0434455eb91859a07b2c589e6034
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-12 00:27:48 +0900

    api folder change

--- src/shared/products.js
+++ src/shared/products.js
@@ -1,2 +1,15 @@
 export const productsLimitFour =
   "http://cozshopping.codestates-seb.link/api/v1/products?count=4";
+
+/**
+"id": 77,
+"type": "Category",
+"title": "나들이",
+"sub_title": null,
+"brand_name": null,
+"price": null,
+"discountPercentage": null,
+"image_url": "https://images.unsplash.com/photo-1567708233986-87de01b33b62?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+"brand_image_url": null,
+"follower": null
+ */
