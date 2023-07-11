8def744ad4d3cbc52d79d64b03492f688b9f7c59
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-07-11 16:02:45 +0900

    이미지 assets 저장

---
+++ client/src/components/main/BookmarkItemList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookmarkItemList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BookmarkItemList;
---
+++ client/src/components/main/MainItemList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainItemList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MainItemList;
