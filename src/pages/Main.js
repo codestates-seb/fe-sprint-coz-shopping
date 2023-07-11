03f8eacf6fc0848c29cea8d9b5d173d7053bc60f
yebin <yebind3308@gmail.com>
2023-07-11 16:30:12 +0900

    📝 푸터 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 
 // 헤더
 const HeaderWhap = styled.div`
-    border : solid 1px black;
+    background: #FFF;
+    box-shadow: 0px 8px 8px 0px rgba(0, 0, 0, 0.10);
     width:1280px;
     height:80px;
     display : flex;
@@ -33,8 +34,8 @@ const HeaderWhap = styled.div`
     }
 `
 
-// 모달
-const Modal = styled.div`
+// 모달 (시멘틱요소:nav)
+const Modal = styled.nav`
     position:relative;
     width:200px;
     height:172px;
@@ -92,6 +93,48 @@ const Modal = styled.div`
     }
 `
 
+// 아티클
+const Article = styled.article`
+
+    .articleWrap {
+        height:694px;
+    }
+
+`
+
+// 푸터
+const Footer = styled.article`
+
+    .footerWrap {
+        height:58px;
+        width:1280px;
+        margin:auto;
+        border-top:solid 1px black;
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+        > span {
+            height:18px;
+            font-size : 12px;
+            color : #888888;
+            display:flex;
+            flex-direction: column;
+            align-items: center;
+        }
+    }
+
+`
+
+
+
+
+
+
+
+
+
+
+
 
 const Main = () => {
     const [ModalOpen, setModalOpen] = useState(false);
@@ -101,42 +144,70 @@ const Main = () => {
     };
 
     return (
-        <header>
-            {/* 헤더 */}
-            <HeaderWhap>
-                <div className="headerLeft">
-                    <img className="leftImg" src="/images/logo.png" alt="코드스테이츠 로고" />
-                    <h1 className="leftText">COZ Shopping</h1>
-                </div>
-                <div className="headerRight" onClick={menuClick}>
-                    <img className="rightImg" src="/images/unnamed.png" alt="메뉴 버튼" />
-                </div>
-            </HeaderWhap>
-
-            {/* 모달 */}
-            {ModalOpen && (
-                <Modal>
-                    <div className="modalImg">
-                        <img className="modalArrow" src="/images/polygon.png" alt="메뉴 버튼 오픈" />
+        <>
+            <header>
+                {/* 헤더 */}
+                <HeaderWhap>
+                    <div className="headerLeft">
+                        <img className="leftImg" src="/images/logo.png" alt="코드스테이츠 로고" />
+                        <h1 className="leftText">COZ Shopping</h1>
                     </div>
-                    <div className="modalWrap">
-                        <div className="modalText">
-                            <span>OOO님, 안녕하세요!</span>
-                        </div>
-
-                        <div className="modalText">
-                            <img className="modalArrow" src="/images/gift.png" alt="상품 리스트 페이지" />
-                            <span>상품리스트 페이지</span>
-                        </div>
-
-                        <div className="modalText">
-                            <img className="modalArrow" src="/images/star.png" alt="북마크 페이지" />
-                            <span>북마크 페이지</span>
-                        </div>
+                    <div className="headerRight" onClick={menuClick}>
+                        <img className="rightImg" src="/images/unnamed.png" alt="메뉴 버튼" />
                     </div>
-                </Modal>
-            )}
-        </header>
+                </HeaderWhap>
+
+                {/* 모달 */}
+                {ModalOpen && (
+                    <Modal>
+                        <div className="modalImg">
+                            <img className="modalArrow" src="/images/polygon.png" alt="메뉴 버튼 오픈" />
+                        </div>
+                        <div className="modalWrap">
+                            <div className="modalText">
+                                <span>OOO님, 안녕하세요!</span>
+                            </div>
+
+                            <div className="modalText">
+                                <img className="modalArrow" src="/images/gift.png" alt="상품 리스트 페이지" />
+                                <span>상품리스트 페이지</span>
+                            </div>
+
+                            <div className="modalText">
+                                <img className="modalArrow" src="/images/star.png" alt="북마크 페이지" />
+                                <span>북마크 페이지</span>
+                            </div>
+                        </div>
+                    </Modal>
+                )}
+            </header>
+
+            <Article>
+                <div className="articleWrap">
+
+                </div>
+            </Article>
+
+            <Footer>
+                <div className="footerWrap">
+                    <span>개인정보 처리방침 | 이용 약관</span>
+                    <span>All rights reserved @ Codestates</span>
+                </div>
+            </Footer>
+        </>
+
+
+
+
+
+
+
+
+
+
+
+
+
     );
 };
 
