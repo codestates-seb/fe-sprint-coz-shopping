8cac491759b23a24d2fc2e48665d728ad2f32d44
astra <astra9706@naver.com>
2023-07-11 14:00:58 +0900

    feat: 불필요한 파일 삭제

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fe-sprint-coz-shopping
+
+# 프로젝트 구조 
+# Header, 메인페이지, 상품리스트페이지, 북마크 페이지, Footer
+
+# Header 요구사항 
+#
---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          제발 되라
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
