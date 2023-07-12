215e9cb885234520778d3f3b4773301e131e07f8
Moses-Yu <mosesyumss@gmail.com>
2023-07-12 14:46:59 +0900

    [#5] get product list from server

--- src/App.js
+++ src/App.js
@@ -3,11 +3,75 @@ import { Routes, Route } from "react-router-dom";
 import MainPage from "./Pages/MainPage";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [all, setAll] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [exhibitions, setExhibitions] = useState([]);
+  const [brands, setBrands] = useState([]);
+
+  const requestInfo = (count) => {
+    axios({
+      method: "get",
+      url: "http://cozshopping.codestates-seb.link/api/v1/products",
+      params:{
+        count
+      }
+    }).then(function (response) {
+      setAll(response.data);
+      console.log(response.data);
+    });
+  }
+  
+  useEffect(() => {
+    requestInfo(10)
+  }, []);
+
+  useEffect(() => {
+    const filterByType = () => {
+      const p = []
+      const e = []
+      const b = []
+      const c = []
+      all.forEach((el) => {
+        const type = el.type;
+        switch (type) {
+          case "Product":
+            p.push(el)
+            break;
+          case "Exhibition":
+            e.push(el)
+            break;
+          case "Brand":
+            b.push(el)
+            break;
+          case "Category":
+            c.push(el)
+            break;
+          default:
+            break;
+        }
+      });
+
+      setProducts(p)
+      setCategories(c)
+      setExhibitions(e)
+      setBrands(b)
+    };
+
+    filterByType();
+    return () => {};
+  }, [all]);
+
   return (
     <div className="App">
       <Header />
+      {all.map((el) => (
+        <div>{el.title}</div>
+      ))}
       <Routes>
         <Route path="/" element={<MainPage></MainPage>} />
         <Route path="/" element={<MainPage></MainPage>} />
