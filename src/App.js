378a26795e0347f946c33e35c99a8fd92755a0dc
JIAHLEE0213 <ja7943@naver.com>
2023-07-12 17:23:08 +0900

    footer, header 구현

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import './App.css'
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Header/>
+          <Routes>
+            <Route path="/"/>
+            <Route path="/products/list"/>
+            <Route path="/bookmark"/>
+          </Routes>
+        <Footer/>  
+      </BrowserRouter>
+  );
+}
+
+export default App;
