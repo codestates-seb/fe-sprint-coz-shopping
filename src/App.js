e28c0ee1690d803e95964c61cdc4ec843822b3cf
rlaxotn2300 <rlaxotn2300@naver.com>
2023-07-12 11:06:59 +0900

    푸터완성 헤더 만드는중

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import Header from './Header'
+import Footer from './Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return(
+    <div>
+      <Header />
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <p>asdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdd</p>
+      <Footer />
+    </div>
+  )
+  // return (
+  //   // <BrowserRouter>
+  //   //   <div className='main'>
+  //   //     <header></header>
+  //   //     <section>
+  //   //       <Routes>
+  //   //         <Route path='/' element={<Section />} />
+  //   //       </Routes>
+  //   //     </section>
+  //   //     <footer></footer>
+  //   //   </div>
+  //   // </BrowserRouter>
+  // )
+}
+
+export default App
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div>
+                <div>
+                    <img src={'../public/images/unnamed.png'} />
+                    <p>COZ Shoppung</p>    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
