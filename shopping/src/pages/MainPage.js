eb15fd5ad3e7672bdbd30912ad5f1253bc2f22b8
udaeng8286 <udaenglvup@gmail.com>
2023-07-12 17:36:24 +0900

    불필요한 파일 삭제

---
+++ shopping/src/pages/MainPage.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import React, { useState } from 'react';
+import Headers from "../component/Headers";
+import Footers from "../component/Footers";
+
+
+// 아티클
+const Article = styled.article`
+    .articleWrap {
+        height:694px;
+    }
+`
+
+
+const MainPage = () => {
+
+    return (
+        <>
+            {/* 헤더 */}
+            <Headers />
+
+            {/* 아티클 */}
+            <Article>
+                <div className="articleWrap">
+
+                </div>
+            </Article>
+
+            {/* 푸터 */}
+            <Footers />
+        </>
+
+    );
+};
+
+export default MainPage;
