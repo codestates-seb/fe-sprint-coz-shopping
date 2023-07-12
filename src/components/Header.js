a6c4ca8db9a5a0ce9e027cf6a0ba92eeb0741e8b
toa <gimhyoungsoo@gmail.com>
2023-07-12 16:15:58 +0900

    Design: Header UI 구현
    - Header.js 버거메뉴 classname 불필요하여 제거

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
 import styles from "../styles/Header.module.css";
+import logo from "../assets/로고.png"
+import name from "../assets/쇼핑몰 이름.png"
+import burger from "../assets/햄버거 버튼.png"
 function Header() {
   return (
     <header>
       <div className={styles.header}>
         <div className={styles.logo_wrap}>
           <div className={styles.inner_logo_image_wrap}>
-            <img src="#" alt="코드스테이츠_이미지" />
+            <img src={logo} alt="코드스테이츠_이미지" />
           </div>
           <div className={styles.inner_logo_text_wrap}>
-            <img src="#" alt="코드스테이츠_텍스트" />
+            <img src={name} alt="코드스테이츠_텍스트" />
           </div>
         </div>
 
-        <div className="burgerMenu">
-          <img src="#" alt="버거메뉴" />
+        <div>
+          <img src={burger} alt="버거메뉴" />
         </div>
       </div>
     </header>
