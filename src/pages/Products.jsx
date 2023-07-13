969dba9eaf99eac433f9395d034e3e7f23d32868
LeeJongBum <qja965@gmail.com>
2023-07-13 11:27:01 +0900

    [#14] 필터링 로직 구현

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import FilterTab from "../components/FilterTab";
-
+import { useSelector } from "react-redux";
+import Card from "../components/Card";
 const Section = styled.section`
   display: "flex";
   margin: auto;
@@ -11,10 +12,26 @@ const Section = styled.section`
   margin-left: 76px;
   margin-right: 76px;
 `;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 24px;
+`;
 export default function Products() {
+  const data = useSelector((state) => state.AllProducts.value);
+  const filterTab = useSelector((state) => state.CurFilter.value.Products);
+  const filterList =
+    filterTab === "All" ? data : data.filter((el) => el.type === filterTab);
   return (
     <Section>
       <FilterTab />
+      <Container>
+        {filterList.map((el, i) => (
+          <Card key={i} item={el} />
+        ))}
+      </Container>
     </Section>
   );
 }
