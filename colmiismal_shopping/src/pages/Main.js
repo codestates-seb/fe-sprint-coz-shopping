59859c51671609f5216aa826ca154bd38d415991
ColmiismaL <colruck32@gmail.com>
2023-07-13 21:53:50 +0900

    리스트에 맞는 css 적당히 맞춤

--- colmiismal_shopping/src/pages/Main.js
+++ colmiismal_shopping/src/pages/Main.js
@@ -3,79 +3,72 @@ import Footer from "../component/Footer"
 
 import "./Main.css"
 
+import { useEffect, useState } from 'react';
+import { getProducts } from '../api/api.js';
+
+
 
 
 export default function Main() {
+
+    // const [flightList, setFlightList] = useState([]);
+
+    // useEffect(() => {
+    //     getProducts()
+    //         .then(data => { setFlightList(data); });
+    //     }, []);
+    //     useEffect(() => {
+    //     console.log(flightList)
+    //     }, [flightList])
+
+
+
+
+
     return (
         <div className="mainin">
             <Header></Header>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
+            <div id="maininin">
+                <div className="listmain"><span>상품 리스트</span>
+                    <ul className="mainul">
+                        <li className="mainbox">
+                            <img className="bookmarkonoff" src="/bookmarkoff.png" alt="" ></img>
+                            <img className="productimage" src="" alt=""></img>
+                            <div className="firsttext">상품이름,카테고리</div>
+                            <div className="secondtext">돈</div>
+                            <div className="thirdtext">할인</div>
+                        </li>
+                        <li className="mainbox">
+                            <img className="bookmarkonoff" src="/bookmarkoff.png" alt="" ></img>
+                            <img className="productimage" src="" alt=""></img>
+                        </li>
+                        <li className="mainbox">
+                            <img className="bookmarkonoff" src="/bookmarkoff.png" alt="" ></img>
+                            <img className="productimage" src="" alt=""></img>
+                        </li>
+                        <li className="mainbox">
+                            <img className="bookmarkonoff" src="/bookmarkoff.png" alt="" ></img>
+                            <img className="productimage" src="" alt=""></img>
+                        </li>
+                    </ul>
+                </div>
+                <div className="listmain"><span>북마크 리스트</span>
+                <ul className="mainul">
+                        <li className="mainbox">
+
+                        </li>
+                        <li className="mainbox">
+
+                        </li>
+                        <li className="mainbox">
+
+                        </li>
+                        <li className="mainbox">
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
             <Footer></Footer>
         </div>
     )
