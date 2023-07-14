c5547a20883b948ec19a5b9ecca4e6f6afc3a278
JIAHLEE0213 <ja7943@naver.com>
2023-07-14 18:10:32 +0900

    상품페이지 api 받아오기, 메인페이지css 작업중

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -4,10 +4,19 @@ import './Product.css'
 export default function Product ({product}) {
     return (
         <div key={product.id} className="product">
-            <img className="product_profile" src={product.image_url} alt="product image"/>
-            <span className="product-title">{product.title}</span>
-            <span className="product-dcRate">{`${product.discountPercentage}%` && null}</span>
-            <span className="product-price">{product.price}</span>
+            {/* 이미지 key이름이 다를때 논리합 연산자로 받아오기 */}
+            <img className="product_profile" src={product.image_url || product.brand_image_url} alt="product image"/>
+            <span className="product-title">
+                {product.type === 'Category' ? `#${product.title}` || `#${product.brand_name}`: product.title || product.brand_name}</span>
+            <span className="product_sub_title">{product.sub_title ? `${product.sub_title}` : null}</span>
+            <span className="product-follower_wrapper">
+                <p className="product_follwer">
+                    {product.type === 'Brand' ? '관심고객수' : null}
+                </p>
+                {product.follower ? `${product.follower} ` : null}
+            </span>
+            <span className="product-dcRate">{product.discountPercentage ? `${product.discountPercentage}%` : null}</span>
+            <span className="product-price">{product.price ? `${product.price}원` : null}</span>
         </div>
     )
 } 
